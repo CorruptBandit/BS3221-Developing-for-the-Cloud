a080676b96d5d770e1160375f5d92a8fbe123328
Oliver Smith <90264544+CorruptBandit@users.noreply.github.com>
2023-10-12 21:47:24 +0100

    Create mongo database (#3)
    * Work on utilising Mongo
    
    * Get the frontend finally talking to the backend
    
    * Get Mongo connection working
    
    * Display the pets when logged in
    
    * Change the UI to allow for dog walkers
    
    ---------
    
    Co-authored-by: Oliver Smith <oliver.smith@roke.co.uk>

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-import markdown
+from typing import List, Optional
 
-from fastapi import Request, status
+from fastapi import Body, Request, status
 from fastapi_offline import FastAPIOffline
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import certifi
+import markdown
 import uvicorn
 
 
@@ -16,40 +21,58 @@ app = FastAPIOffline(
     title="Backend",
     description="API to talk to MongoDB",
     root_path="",
-    version = "development"
+    version = "development",
+    docs_url=None
 )
 
 config = {
     "fastapi_host": "0.0.0.0",
     "fastapi_port": 8080,
-    "proxy_path": "",  # Empty string if not behind a reverse proxy
-
+    "proxy_path" : ""
 }
 
 template_dir = SCRIPT_DIR / "templates"
 templates = Jinja2Templates(directory=template_dir)
 
+# Create a model to represent the user data
+class User(BaseModel):
+    email: str
+    password: str
+
+# Create a model to represent the dog data
+class Dog(BaseModel):
+    owner: str
+    breed: str
+    name: str
+    age: str
+
+uri = "mongodb+srv://University:Winchester123@bs3221.gurmf48.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+# Send a ping to confirm a successful connection
+
+
+users_collection = client["DogCompany"]["Users"]
+dogs_collection = client["DogCompany"]["Dogs"]
+
+async def save_user(user: User):
+    users_collection.insert_one(user.model_dump())
+
+async def save_dog(dog: Dog):
+    dogs_collection.insert_one(dog.model_dump())
 
 @app.get("", include_in_schema=False, status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 @app.get("/", include_in_schema=False, status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 async def redirect_typer():
-    return RedirectResponse(f"{config['proxy_path']}/readme")
+    return RedirectResponse(f"{config['proxy_path']}/index")
 
 
-@app.get("/readme", response_class=HTMLResponse,
+@app.get("/index", response_class=HTMLResponse,
          status_code=status.HTTP_200_OK,
-         summary="Return markdown of readme",
+         summary="Return the index.html",
          include_in_schema=False)
 async def index(request: Request):
-    with open(SCRIPT_DIR / "README.md", "r", encoding="utf-8") as input_file:
-        text = input_file.read()
-
-    html = markdown.markdown(text, extensions=['tables'])
-    data = {
-        "text": html
-    }
     return templates.TemplateResponse("/index.html", context={"request": request})
-
 
 @app.get("/status", include_in_schema=False)
 async def get_status():
@@ -59,6 +82,32 @@ async def get_status():
     Return status code
     """
     return 200
+
+@app.post("/register", include_in_schema=False)
+async def register(user: User = Body(...), dogs: List[Dog] = Body(...)):
+    await save_user(user)
+    for dog in dogs:
+        await save_dog(dog)
+
+
+@app.get("/user", include_in_schema=False)
+async def get_user(request: Request, email: str):
+    """
+    Get user details and their dogs
+    """
+    user = users_collection.find_one({"email": email})
+
+    if not user:
+        return JSONResponse(content={"message": "User not found"}, status_code=status.HTTP_404_NOT_FOUND)
+
+    # Get the user's dogs associated with their email
+    user_dogs = dogs_collection.find({"owner": email})
+
+    dog_list = []
+    for dog in user_dogs:
+        dog_list.append({"name": dog["name"], "age": dog["age"], "breed": dog["breed"]})
+
+    return templates.TemplateResponse("user_info.html", {"request": request, "email": email, "dogs": dog_list})
 
 
 def main():
