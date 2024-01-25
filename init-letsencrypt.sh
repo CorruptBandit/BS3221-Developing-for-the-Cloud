3efc33b8b470291e9d7b9c477af4608f3ab28982
Oliver Smith <o.g.smith@icloud.com>
2024-01-25 17:43:08 +0000

    Fix PEM Docker Volume issue

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(legsmuttsmove.co.uk www.legsmuttsmove.co.uk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="o.g.smith@icloud.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -30,6 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
+touch ./data/mongo/mongodb.pem  # Must be created otherwise Docker volumes will create an empty dir
 docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
